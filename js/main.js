465424a4f39cda605491cf1737a83cebecf16d89
kunakatadiwanashe <77622534+kunakatadiwanashe@users.noreply.github.com>
2021-12-24 13:06:33 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).on('ready', function() {
+     
+
+  $(".top-selling").slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 2,
+   
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode:false,
+        }
+      }
+      
+    ]
+   
+    
+  });
+
+});
+
+
+
+
+
